8caa38ab375dff1763b811fd2887d5ac211787c5
jacobdejean <jacobf.dejean@gmail.com>
2023-05-16 18:22:16 -0500

    replacing

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-        name: 'password-generator-app',
-        filename: 'remoteEntry.js',
-        exposes: {
-          './App': './src/App',
-        },
-        shared: ['react', 'react-dom'],
-    })
-  ],
+  plugins: [react()],
   build: {
     target: 'esnext'
   }
